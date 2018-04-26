0cc8655c21329e66a1a31091949e6839d55f1daf
Thomas <m6f7262@gmail.com>
2018-04-26 17:47:43 +0000

    Remove uneeded javascript

--- default/public/js/main.js
+++ default/public/js/main.js
@@ -367,46 +367,7 @@ function content(file) {
 		req.onreadystatechange = function(e) {
 			function err() {
 				if (cancelled) return self.setMessageState('Cancelled', 'error')
-				self.setMessageState(req.statusText || {
-					200: 'OK',
-					201: 'Created',
-					202: 'Accepted',
-					203: 'Non-Authoritative Information',
-					204: 'No Content',
-					205: 'Reset Content',
-					206: 'Partial Content',
-					300: 'Multiple Choices',
-					301: 'Moved Permanently',
-					302: 'Found',
-					303: 'See Other',
-					304: 'Not Modified',
-					305: 'Use Proxy',
-					307: 'Temporary Redirect',
-					400: 'Bad Request',
-					401: 'Unauthorized',
-					402: 'Payment Required',
-					403: 'Forbidden',
-					404: 'Not Found',
-					405: 'Method Not Allowed',
-					406: 'Not Acceptable',
-					407: 'Proxy Authentication Required',
-					408: 'Request Timeout',
-					409: 'Conflict',
-					410: 'Gone',
-					411: 'Length Required',
-					412: 'Precondition Failed',
-					413: 'Request Entity Too Large',
-					414: 'Request-URI Too Long',
-					415: 'Unsupported Media Type',
-					416: 'Requested Range Not Satisfiable',
-					417: 'Expectation Failed',
-					500: 'Internal Server Error',
-					501: 'Not Implemented',
-					502: 'Bad Gateway',
-					503: 'Service Unavailable',
-					504: 'Gateway Timeout',
-					505: 'HTTP Version Not Supported'
-				}[req.status] || 'Unknown error', 'error');
+				self.setMessageState(req.statusText || req.status || 'Unknown error', 'error');
 			}
 			// If we haven't downloaded the headers yet and the request finished
 			// then show an error.
