c76f989a60a769a650181da98b1f51f4ac7ade91
Thomas <m6f7262@gmail.com>
2018-05-01 02:01:37 +0000

    Add input file uploading

--- default/public/js/main.js
+++ default/public/js/main.js
@@ -398,6 +398,10 @@
 		processTransfer(e.clipboardData);
 	});
 
+	document.querySelector('input').addEventListener('change', function() {
+		processFiles(this.files);
+	});
+
 	// https://github.com/odyniec/tinyAgo-js
 	function ago(v){v=0|(Date.now()-v)/1e3;var a,b={second:60,minute:60,hour:24,day:7,week:4.35,month:12,year:1e4},c;for(a in b){c=v%b[a];if(!(v=0|v/b[a]))return c+' '+(c-1?a+'s':a)}}
 
