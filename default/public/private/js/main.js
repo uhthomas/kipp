f39ccced909dd71fb4ae832943f0fab37218a935
Thomas <m6f7262@gmail.com>
2018-05-01 15:00:39 +0000

    Continue to implement async/await

--- default/public/private/js/main.js
+++ default/public/private/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(async function() {
     async function decrypt(iv, key, data) {
         const k = await crypto.subtle.importKey('raw', key, { name: 'AES-GCM' }, false, ['decrypt']);
         return crypto.subtle.decrypt({ name: 'AES-GCM', iv: iv }, k, data);
@@ -10,7 +10,7 @@
 
     function err(message) {
         document.body.className = 'error';
-        document.querySelector('.status .text.open').innerHTML = 'message' in e ? e.message : e;
+        document.querySelector('.status .text.open').innerHTML = 'message' in message ? message.message : message;
     }
 
     try {
@@ -24,50 +24,51 @@
         iv.set(b.slice(0, 12));
         key.set(b.slice(12, 28));
 
-        // XMLHttpRequest instead of fetch since there is no progress reporting with fetch
-        var req = new XMLHttpRequest();
-        req.onprogress = function(e) {
-            document.querySelector('.progress .bar').style.width = ~~(e.loaded / e.total * 100) + '%';
+        const [result, contentType, contentDisposition] = await new Promise(function(resolve, reject) {
+            var req = new XMLHttpRequest();
+            req.onerror = reject;
+            req.onload = function() {
+                this.onprogress = null;
+                if (this.status === 200) resolve([this.response, this.getResponseHeader('Content-Type'), this.getResponseHeader('Content-Disposition')]);
+                else reject(new Error('File has expired or is invalid'));
+            }
+            req.onprogress = function(e) {
+                document.querySelector('.progress .bar').style.width = ~~(e.loaded / e.total * 100) + '%';
+            }
+            req.open('GET', '/' + s[1], true);
+            req.responseType = 'arraybuffer';
+            req.send();
+        });
+
+        document.querySelector('.progress .bar').style.width = '100%';
+        document.querySelector('.status .text.open').innerHTML = 'Decrypting';
+
+        const blob = new Blob([await decrypt(iv, key, result)], { type: contentType })
+        const u = URL.createObjectURL(blob);
+        const name = decodeURIComponent(contentDisposition.split('"')[1].split('"')[0]);
+        document.body.className = 'done';
+        document.querySelector('.status .text.open').innerHTML = 'Open';
+
+        var d = document.createElement('div');
+        d.className = 'info';
+        d.innerHTML = '<div class="name">' + name + '</div>&nbsp;<span class="size">' + filesize(blob.size) + '</span>';
+        document.querySelector('.status').appendChild(d);
+
+        var a = document.querySelector('.status .text.open');
+        a.href = u;
+        var suggested = false;
+        a.onclick = function() {
+            window.open(u, '_blank').onunload = function() {
+                if (suggested) return;
+                suggested = true;
+                document.body.classList.add('suggest');
+            }
+            return false;
         }
-        req.onload = async function() {
-            this.onprogress = null;
 
-            if (this.status !== 200) return err('File has expired or is invalid');
+        var ad = document.querySelector('.status .text.download');
+        ad.href = u;
+        ad.download = name;
 
-            document.querySelector('.progress .bar').style.width = '100%';
-            document.querySelector('.status .text.open').innerHTML = 'Decrypting';
-
-            try {
-                const b = new Blob([await decrypt(iv, key, this.response)], { type: req.getResponseHeader('Content-Type') })
-                const u = URL.createObjectURL(b);
-                const name = decodeURIComponent(req.getResponseHeader('Content-Disposition').split('"')[1].split('"')[0]);
-                document.body.className = 'done';
-                document.querySelector('.status .text.open').innerHTML = 'Open';
-
-                var d = document.createElement('div');
-                d.className = 'info';
-                d.innerHTML = '<div class="name">' + name + '</div>&nbsp;<span class="size">' + filesize(b.size) + '</span>';
-                document.querySelector('.status').appendChild(d);
-
-                var a = document.querySelector('.status .text.open');
-                a.href = u;
-                var suggested = false;
-                a.onclick = function() {
-                    window.open(u, '_blank').onunload = function() {
-                        if (suggested) return;
-                        suggested = true;
-                        document.body.classList.add('suggest');
-                    }
-                    return false;
-                }
-
-                var ad = document.querySelector('.status .text.download');
-                ad.href = u;
-                ad.download = name;
-            } catch (e) { err(e); }
-        }
-        req.open('GET', '/' + s[1], true);
-        req.responseType = 'arraybuffer';
-        req.send();
     } catch(e) { err(e); }
 })();
