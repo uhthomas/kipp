c6efecda15b064c9c34f35999d1c26553e54f25c
Thomas <9749173+uhthomas@users.noreply.github.com>
2020-04-29 12:45:09 +0100

    cleanup local fs

---
+++ filesystem/local/writer.go
@@ -0,0 +1,44 @@
+package local
+
+import (
+	"fmt"
+	"os"
+)
+
+type writer struct {
+	f      *os.File
+	name   string
+	synced bool
+}
+
+// Write writes b to f.
+func (w *writer) Write(b []byte) (n int, err error) { return w.f.Write(b) }
+
+// Sync links the named writer, removes the old link and syncs.
+func (w *writer) Sync() error {
+	name := w.f.Name()
+	if err := os.Link(name, w.name); err != nil && !os.IsExist(err) {
+		return fmt.Errorf("link: %w", err)
+	}
+	if err := os.Remove(name); err != nil {
+		return fmt.Errorf("remove: %w", err)
+	}
+	if err := w.f.Sync(); err != nil {
+		return fmt.Errorf("sync: %w", err)
+	}
+	w.synced = true
+	return nil
+}
+
+// Close closes the underlying writer, and if not synced, removes it.
+func (w *writer) Close() error {
+	if err := w.f.Close(); err != nil {
+		return fmt.Errorf("close: %w", err)
+	}
+	if !w.synced {
+		if err := os.Remove(w.f.Name()); err != nil {
+			return fmt.Errorf("remove: %w", err)
+		}
+	}
+	return nil
+}
