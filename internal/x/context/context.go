3ed3a2a92f943cfe3f258bc74c7fb324c07f9aae
Thomas <9749173+uhthomas@users.noreply.github.com>
2020-10-20 21:00:32 +0100

    gofmt

--- internal/x/context/context.go
+++ internal/x/context/context.go
@@ -9,6 +9,6 @@ type detachedContext struct{ context.Context }
 
 func Detach(ctx context.Context) context.Context { return detachedContext{Context: ctx} }
 
-func (detachedContext) Deadline() (time.Time, bool)       { return time.Time{}, false }
-func (detachedContext) Done() <-chan struct{}             { return nil }
-func (detachedContext) Err() error                        { return nil }
+func (detachedContext) Deadline() (time.Time, bool) { return time.Time{}, false }
+func (detachedContext) Done() <-chan struct{}       { return nil }
+func (detachedContext) Err() error                  { return nil }
